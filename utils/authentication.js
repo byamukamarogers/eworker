89718f535336da3d9aeecd09977001dc05f417d9
ROGERS BYAMUKAMA <byamukamarogers@gmail.com>
2021-03-04 14:20:39 +0300

    finale

--- utils/authentication.js
+++ utils/authentication.js
@@ -8,7 +8,7 @@ module.exports.authenticate = function (req, res, next) {
     if (req.isAuthenticated()) {
         next();
     } else {
-        res.redirect('/login');
+        res.redirect('/home');
         //next();
     }
 }
